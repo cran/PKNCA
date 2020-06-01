f9d35a0083817eda3ed9f2be31c345cae90fbee5
Bill Denney <wdenney@humanpredictions.com>
2020-06-01 16:00:02 +0000

    version 0.9.4

--- inst/doc/AUC-Calculation-with-PKNCA.R
+++ inst/doc/AUC-Calculation-with-PKNCA.R
@@ -15,14 +15,14 @@ scale_fill_discrete <- scale_fill_hue
 my_conc <- data.frame(conc=c(0, 2.5, 3, 2, 1.5, 1.2, 1.1, 0, 0),
                       time=c(0:5, 8, 12, 24),
                       subject=1)
-my_conc$BLOQ <- my_conc$conc == 0
+my_conc$BLQ <- my_conc$conc == 0
 my_conc$measured <- TRUE
 
 ## ----setup-visualization, eval=requireNamespace("ggplot2")--------------------
 ggplot(my_conc,
        aes(x=time,
            y=conc,
-           shape=BLOQ,
+           shape=BLQ,
            group=subject)) +
   geom_line() +
   geom_point(size=4) +
@@ -52,7 +52,7 @@ my_conc$include_auclast <- my_conc$time <= tlast
 ggplot(my_conc,
        aes(x=time,
            y=conc,
-           shape=BLOQ,
+           shape=BLQ,
            group=subject)) +
   geom_ribbon(data=my_conc[my_conc$include_auclast,],
               aes(ymin=0, ymax=conc),
@@ -72,7 +72,7 @@ my_conc$include_aucall <- my_conc$time <= first_after_tlast
 ggplot(my_conc,
        aes(x=time,
            y=conc,
-           shape=BLOQ,
+           shape=BLQ,
            group=subject)) +
   geom_ribbon(data=my_conc[my_conc$include_aucall,],
               aes(ymin=0, ymax=conc),
@@ -89,24 +89,24 @@ my_conc <-
         data.frame(conc=NA,
                    time=36,
                    subject=1,
-                   BLOQ=NA,
+                   BLQ=NA,
                    measured=FALSE,
                    include_auclast=FALSE,
                    include_aucall=FALSE))
 # Extrapolate concentrations for aucinf.obs
 my_conc$conc_aucinf.obs <- my_conc$conc
-my_conc$conc_aucinf.obs[my_conc$BLOQ | is.na(my_conc$BLOQ)] <-
+my_conc$conc_aucinf.obs[my_conc$BLQ | is.na(my_conc$BLQ)] <-
   interp.extrap.conc(conc=my_conc$conc,
                      time=my_conc$time,
-                     time.out=my_conc$time[my_conc$BLOQ | is.na(my_conc$BLOQ)],
+                     time.out=my_conc$time[my_conc$BLQ | is.na(my_conc$BLQ)],
                      lambda.z=as.data.frame(results_obj)$PPORRES[as.data.frame(results_obj)$PPTESTCD %in% "lambda.z"])
 
 # Extrapolate concentrations for aucinf.pred
 my_conc$conc_aucinf.pred <- my_conc$conc
-my_conc$conc_aucinf.pred[my_conc$BLOQ | is.na(my_conc$BLOQ)] <-
+my_conc$conc_aucinf.pred[my_conc$BLQ | is.na(my_conc$BLQ)] <-
   interp.extrap.conc(conc=my_conc$conc,
                      time=my_conc$time,
-                     time.out=my_conc$time[my_conc$BLOQ | is.na(my_conc$BLOQ)],
+                     time.out=my_conc$time[my_conc$BLQ | is.na(my_conc$BLQ)],
                      lambda.z=as.data.frame(results_obj)$PPORRES[as.data.frame(results_obj)$PPTESTCD %in% "lambda.z"],
                      clast=as.data.frame(results_obj)$PPORRES[as.data.frame(results_obj)$PPTESTCD %in% "clast.pred"])
 my_conc$conc_aucinf.pred[my_conc$time == tlast] <-
@@ -116,7 +116,7 @@ my_conc$conc_aucinf.pred[my_conc$time == tlast] <-
 ggplot(my_conc[!is.na(my_conc$conc),],
        aes(x=time,
            y=conc,
-           #shape=BLOQ,
+           #shape=BLQ,
            group=subject)) +
   geom_ribbon(data=my_conc,
               aes(ymin=0, ymax=conc_aucinf.obs),
@@ -130,7 +130,7 @@ ggplot(my_conc[!is.na(my_conc$conc),],
 ggplot(my_conc[!is.na(my_conc$conc),],
        aes(x=time,
            y=conc,
-           #shape=BLOQ,
+           #shape=BLQ,
            group=subject)) +
   geom_ribbon(
     data=arrange(
